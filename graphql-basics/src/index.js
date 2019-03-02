d5626c86ab8902127eeec70c7b39a0ee73aec546
Eduardo Verdeja <eaverdeja@gmail.com>
2019-03-02 19:17:06 -0300

    Named and default exports

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,6 @@
-console.log('Hello World!')
+import { message } from './myModule'
+import add, { sub } from './math'
+
+console.log('Message from myModule.js: ', message)
+console.log('Default export from math.js - add: ', `2 + 2 = ${add(2, 2)}`)
+console.log('Named export from math.js - sub: ', `2 - 1 = ${sub(2, 1)}`)
