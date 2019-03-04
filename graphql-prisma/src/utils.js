039968b38257064ba5189fef03681556f8a7b1c5
Eduardo Verdeja <eaverdeja@gmail.com>
2019-03-04 17:08:18 -0300

    Wrapping the call to console.log(JSON.stringify())

---
+++ graphql-prisma/src/utils.js
@@ -0,0 +1,13 @@
+export const prettyLog = (data, label = null) => {
+    if (label) {
+        console.groupCollapsed(label)
+    }
+
+    console.log(JSON.stringify(data, null, 3))
+
+    if (label) {
+        console.groupEnd()
+    }
+
+    return Promise.resolve(data)
+}
