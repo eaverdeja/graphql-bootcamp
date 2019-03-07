6a7b499c86c701e4e6bea562e19185e6c2c73ecc
Eduardo Verdeja <eaverdeja@gmail.com>
2019-03-07 06:50:18 -0300

    Making auth optional (conditional error throwing)

--- graphql-prisma/src/utils/auth.js
+++ graphql-prisma/src/utils/auth.js
@@ -2,18 +2,22 @@ import jwt from 'jsonwebtoken'
 
 const JWT_SECRET = 'thisisasecret'
 
-export const getUserId = request => {
+export const getUserId = (request, requireAuth = true) => {
   const { request: parsedRequest } = request
   const authHeader = parsedRequest.headers.authorization
 
-  if (!authHeader) {
+  if (authHeader) {
+    const token = authHeader.replace('Bearer ', '')
+    const decoded = jwt.verify(token, JWT_SECRET)
+
+    return decoded.userId
+  }
+
+  if (requireAuth) {
     throw new Error('No auth token was given')
   }
 
-  const token = authHeader.replace('Bearer ', '')
-  const decoded = jwt.verify(token, JWT_SECRET)
-
-  return decoded.userId
+  return null
 }
 
 export const createToken = payload => jwt.sign(payload, JWT_SECRET)
