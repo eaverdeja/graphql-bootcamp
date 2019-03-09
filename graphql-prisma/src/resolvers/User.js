b29cbcdab8a8a9a53a9a78911bcd7cc362426416
Eduardo Verdeja <eaverdeja@gmail.com>
2019-03-09 09:22:52 -0300

    Locking down individual fields

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,3 +1,21 @@
-const User = {}
+const User = {
+  async email(
+    parent,
+    args,
+    {
+      request,
+      auth: { getUserId }
+    },
+    info
+  ) {
+    const userId = getUserId(request, false)
+
+    if (userId === parent.id) {
+      return parent.email
+    }
+
+    return null
+  }
+}
 
 export { User as default }
