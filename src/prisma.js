6685667289cd930a078b101c9f369b10f2287f34
Eduardo Verdeja <eaverdeja@gmail.com>
2019-03-16 20:52:15 -0300

    Extracting the Prisma secret from the codebase

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers'
 const prisma = new Prisma({
   typeDefs: 'src/generated/schema.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'somethingsupersecret',
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 })
 
